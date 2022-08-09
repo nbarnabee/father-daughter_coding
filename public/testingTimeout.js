ca152a4e35eb8d44d81db4774c475d57f1b55d14
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-08-09 09:42:07 +0200

    Tweak messages and display timing

--- public/testingTimeout.js
+++ public/testingTimeout.js
@@ -19,7 +19,6 @@ document.querySelector(".set-reminder").onclick = () => {
   const reminder = () => {
     alert("This is your reminder!");
     textBox1.classList.toggle("text-box-awake");
-    textBox2.classList.toggle("text-box-awake");
   };
   setTimeout(reminder, timeToReminder);
   modal.classList.toggle("modal-open");
@@ -27,8 +26,21 @@ document.querySelector(".set-reminder").onclick = () => {
 };
 
 function displayText() {
+  textBox1.innerText =
+    "Natively, JS can only 'do' one thing at a time. The neat thing about promises is that they allow us to overcome that limitation. Other functions (like the one that's causing this text to be displayed) can run while we wait for the timer function to resolve.";
   textBox1.classList.toggle("text-box-awake");
   setTimeout(() => {
     textBox2.classList.toggle("text-box-awake");
-  }, 4000);
+    displayText2();
+  }, 6000);
+}
+
+function displayText2() {
+  // textBox1.classList.toggle("text-box-awake");
+  setTimeout(() => {
+    textBox1.innerText =
+      "You could also enter data into the forms and submit them while we continue to wait for the timer.  Without asynchronous programming, that wouldn't be possible.";
+    // textBox1.classList.toggle("text-box-awake");
+    textBox2.classList.toggle("text-box-awake");
+  }, 5000);
 }
