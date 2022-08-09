2e9745711c8fd87d82b8709592610dc8fe0d4ad2
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-08-09 09:22:57 +0200

    Build and style asynchronous section

--- public/testingTimeout.js
+++ public/testingTimeout.js
@@ -11,11 +11,24 @@ modalControllers.forEach((a) =>
   })
 );
 
+const textBox1 = document.querySelector(".text-box1"),
+  textBox2 = document.querySelector(".text-box2");
+
 document.querySelector(".set-reminder").onclick = () => {
   const timeToReminder = document.getElementById("reminder").value * 1000;
   const reminder = () => {
     alert("This is your reminder!");
+    textBox1.classList.toggle("text-box-awake");
+    textBox2.classList.toggle("text-box-awake");
   };
   setTimeout(reminder, timeToReminder);
   modal.classList.toggle("modal-open");
+  displayText();
 };
+
+function displayText() {
+  textBox1.classList.toggle("text-box-awake");
+  setTimeout(() => {
+    textBox2.classList.toggle("text-box-awake");
+  }, 4000);
+}
